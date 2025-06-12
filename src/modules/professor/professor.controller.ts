81bbbc99786806dab5ea0d426d923e1dec3b933b
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-06-12 16:40:52 -0300

    feat: implement file upload and download functionality for professor data

--- src/modules/professor/professor.controller.ts
+++ src/modules/professor/professor.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express';
 import * as professorService from './professor.service';
+import path from 'path'
+import { uploadProfessorFile } from '../../utils/xlsx';
+
 
 export async function create(req: Request, res: Response) {
   try {
@@ -10,10 +13,19 @@ export async function create(req: Request, res: Response) {
   }
 }
 
+export async function createMany(req: Request, res: Response) {
+  try {
+    const professors = await professorService.createManyProfessors(req.body)
+    return res.status(201).json({ mensagem: "Professores criados com sucesso", data: professors })
+  } catch (error) {
+    return res.status(400).json({ mensagem: "Erro ao criar professor", erro: (error as Error).message });
+  }
+}
+
 export async function getAll(req: Request, res: Response) {
   try {
     const professores = await professorService.getAllProfessores();
-    if (!professores || professores.length === 0) 
+    if (!professores || professores.length === 0)
       return res.status(404).json({ mensagem: 'Nenhum professor encontrado' });
     return res.status(200).json({ mensagem: "Professores encontrados com sucesso", data: professores });
   } catch (error) {
@@ -38,7 +50,7 @@ export async function update(req: Request, res: Response) {
 
     const professor = await professorService.getProfessorById(id);
     if (!professor) return res.status(404).json({ mensagem: 'Professor não encontrado' });
-    
+
     const professorAtualizado = await professorService.updateProfessor(id, req.body);
     return res.status(200).json({ mensagem: "Professor atualizado com sucesso", data: professorAtualizado });
   } catch (error) {
@@ -53,7 +65,7 @@ export async function remove(req: Request, res: Response) {
     const professor = await professorService.getProfessorById(id);
     if (!professor) return res.status(404).json({ mensagem: 'Professor não encontrado' });
 
-    if (professor.materias.length > 0) 
+    if (professor.materias.length > 0)
       return res.status(400).json({ mensagem: "Não é possível deletar professor com matérias associadas" });
 
     if (professor.cursoCoordenado)
@@ -63,5 +75,36 @@ export async function remove(req: Request, res: Response) {
     return res.status(204).json({ mensagem: "Professor deletado com sucesso" });
   } catch (error) {
     return res.status(400).json({ mensagem: "Erro ao deletar professor", erro: (error as Error).message });
+  }
+}
+
+//download professor xsl file
+export async function downloadFile(req: Request, res: Response) {
+  try {
+    const filePath = path.join(__dirname, "../../../public/planilha-modelo.xlsx")
+    res.download(filePath, "planilha-modelo.xlsx")
+  } catch (error) {
+    return res.status(400).json({ mensagem: `${(error as Error).message}` })
+  }
+}
+
+export async function uploadFile(req: Request, res: Response) {
+  try {
+    const buffer = req.file?.buffer
+
+    if (!buffer) {
+      res.status(400).json({ message: "Arquivo nao encontrado" })
+      return
+    }
+
+    const response = await uploadProfessorFile(buffer)
+
+    res.status(201).send({
+      message: "Produtos Cadastrados com Sucesso! 🥳🎉",
+      response
+    })
+
+  } catch (error) {
+    return res.status(400).json({ mensagem: `${(error as Error).message}` })
   }
 }
--- src/modules/professor/professor.routes.ts
+++ src/modules/professor/professor.routes.ts
@@ -1,14 +1,25 @@
 import { Router } from 'express';
-import { create, getAll, getById, update, remove } from './professor.controller';
+import { create, getAll, getById, update, remove, downloadFile, uploadFile } from './professor.controller';
 import { validateBody } from '../../middlewares/validate.middleware';
 import { createProfessorSchema, updateProfessorSchema } from '../../schemas/professor.schema';
 import { authenticateToken } from '../../middlewares/auth.middleware';
+
+import multer from 'multer'
 
 const router = Router();
 
 router.post('/', (req, res) => {
     authenticateToken(req, res, () => { validateBody(createProfessorSchema)(req, res, () => { create(req, res) }) });
 });
+
+const upload = multer({ storage: multer.memoryStorage() })
+router.post('/upload/xsl', upload.single('file'), (req, res) => {
+    authenticateToken(req, res, () => { uploadFile(req, res) })
+})
+
+router.get('/download/xsl', (req, res) => {
+    authenticateToken(req, res, () => { downloadFile(req, res) })
+})
 
 router.get('/', (req, res) => {
     authenticateToken(req, res, () => { getAll(req, res) });
