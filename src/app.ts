fb80b4eb60903a5d54240b0640890889b4201e18
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-06-16 16:37:31 -0300

    feat: serve static files from the public directory

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import usuarioRoutes from './modules/usuario/usuario.routes';
 import authRoutes from './modules/auth/auth.routes';
 import enumRoutes from './modules/enums/enum.routes';
 
+import path from 'path';
 import cors from 'cors';
 
 import swaggerUi from 'swagger-ui-express';
@@ -22,6 +23,7 @@ app.use('/materias', materiaRoutes);
 app.use('/usuarios', usuarioRoutes);
 app.use('/auth', authRoutes);
 app.use('/enums', enumRoutes);
+app.use('/static', express.static(path.join(__dirname, '..', 'public')))
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
