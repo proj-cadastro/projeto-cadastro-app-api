338e2eb33f6232710a24cb53269e02428e291dea
Vitor Ruan Firmino de Oliveira <110908588+VitorRuan@users.noreply.github.com>
2025-06-25 10:03:45 -0300

    Update migration.sql

--- prisma/migrations/20250608040435_cascada_curso_materia/migration.sql
+++ prisma/migrations/20250608040435_cascada_curso_materia/migration.sql
@@ -1,5 +1,5 @@
 -- DropForeignKey
-ALTER TABLE `Cursomateria` DROP FOREIGN KEY `CursoMateria_cursoId_fkey`;
+ALTER TABLE `CursoMateria` DROP FOREIGN KEY `CursoMateria_cursoId_fkey`;
 
 -- AddForeignKey
 ALTER TABLE `CursoMateria` ADD CONSTRAINT `CursoMateria_cursoId_fkey` FOREIGN KEY (`cursoId`) REFERENCES `Curso`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
